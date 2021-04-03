353c3f74f065c0cbaf14271425edffad4c251008
Jorge <jepers2021@gmail.com>
2021-04-03 11:05:01 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace A133590.Ejercicio42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 42");
+            Console.WriteLine("Programa que recibe una palabra y la presenta al revés.");
+            Console.Write("Por favor, ingrese una palabra: ");
+            char[] palabra = Console.ReadLine().ToCharArray();
+            Array.Reverse(palabra);
+            Console.WriteLine($"Palabra invertida: {new string(palabra)}");
+            Console.WriteLine("Presione cualquier tecla para continuar..");
+            Console.ReadKey();
+        }
+    }
+}
